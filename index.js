6a44700e071ccb2036c07e9d474f7f48f7d5a1a5
Martin Stone <martin@pagerduty.com>
2017-06-26 15:04:43 -0400

    formatting

--- index.js
+++ index.js
@@ -326,7 +326,7 @@ app.post('/whatsapp2', function(req, res) {
 	
 	var message = req.body.messages[0];
 	
-	var wa_message = 'Incident Title: ' + message.incident.summary + '\nEvent: ' + message_type_strings[message.event] + '\nBy: ' + message.incident.last_status_change_by.summary + '\nService: '  + message.incident.service.name + '\nURL: ' + message.incident.html_url;
+	var wa_message = 'Incident Title: ' + message.incident.summary.replace(/\\n/, '\n') + '\nEvent: ' + message_type_strings[message.event] + '\nBy: ' + message.incident.last_status_change_by.summary + '\nService: '  + message.incident.service.name + '\nURL: ' + message.incident.html_url;
 	
 	var body = {
 		'group_admin': group_admin,
