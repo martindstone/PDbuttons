cc75ff139cbaa3728bb8dd49ace8f302ab80337c
Martin Stone <martin@pagerduty.com>
2017-07-31 15:58:02 -0400

    pd-pd

--- index.js
+++ index.js
@@ -320,16 +320,19 @@ app.post('/pingdom', function(req, res) {
 	var pingdom_user = req.query.pingdom_user;
 	var pingdom_pass = req.query.pingdom_pass;
 	var pingdom_token = req.query.pingdom_token;
+	var event = req.body.messages[0].event;
 	
 
 	console.log(req.body);
 	getTriggerLE(token, incident.first_trigger_log_entry.self, function(logEntry) {
-		console.log("event type: " + req.body.messages[0].event);
+		console.log("event type: " + event );
 		var pingdom_args, note;
-		if ( action == "pause" || req.body.messages[0].event == 'incident.acknowledge' ) {
+		if ( action == "pause" || event == 'incident.acknowledge' ) {
+			console.log("pause the check");
 			pingdom_args = "paused=true";
 			note = "Paused check " + logEntry.log_entry.channel.incident_key + ". Will unpause when the incident is resolved.";
-		} else if ( action == "unpause" || req.body.messages[0].type == 'incident.resolve' ) {
+		} else if ( action == "unpause" || event == 'incident.resolve' ) {
+			console.log("unpause the check");
 			pingdom_args = "paused=false";
 			note = "Unpaused check " + logEntry.log_entry.channel.incident_key;
 		} else {
