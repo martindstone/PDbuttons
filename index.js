3a87c73ce9c240aaedb6db7818494aa730ad1857
Martin Stone <martin@pagerduty.com>
2017-10-03 17:08:02 -0400

    slack

--- index.js
+++ index.js
@@ -479,6 +479,7 @@ app.post('/slackuser', function(req, res) {
 		request(options, function(error, response, body) {
 			if ( ! response.statusCode || response.statusCode < 200 || response.statusCode > 299 ) {
 				console.log("Error creating incident: " + error + "\nResponse: " + JSON.stringify(response, null, 2) + "\nBody: " + JSON.stringify(body, null, 2));
+				sendSlackResponse(responseURL, `Couldn't create incident - error ${error.statusCode} from PagerDuty incidents endpoint`);
 			} else {
 				var response = { 
 					response_type: "ephemeral", 
@@ -490,7 +491,8 @@ app.post('/slackuser', function(req, res) {
 						} 
 					] 
 				}
-				sendSlackResponse(responseURL, response);			}
+				sendSlackResponse(responseURL, response);
+			}
 		});	
 	});
 });
